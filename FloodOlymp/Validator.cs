9762a0fc427e64669f9b2d84a0767df7390cd6e9
Анисимов Кирилл <k.anisimov@maximaster.ru>
2017-07-17 14:19:34 +0300

    Rename the classes

---
+++ FloodOlymp/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodOlymp
+{
+    static class Validator
+    {
+
+        public static void CheckBorder(int number, int min, int max, string message)
+        {
+            if (number < min || number > max)
+                throw new Exception(message);
+        }
+
+
+    }
+}
